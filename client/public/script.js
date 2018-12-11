1d3f72c65b4216d74102c61496f45e525fb23920
e.e.moir <e.e.moir@gmail.com>
2018-12-11 14:22:08 -0500

    script modifications

--- client/public/script.js
+++ client/public/script.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 	document.querySelector("#form-submit").onsubmit = (event) => {
 		event.preventDefault()
+		const xhr = new XMLHttpRequest()
+		let name = document.querySelector("#name").value
+		let email = document.querySelector("#email").value
+		let phone = document.querySelector("#phone").value
+		let message = document.querySelector("#message").value
+		request.open("POST", "/", true)
+		const data = new FormData()
+		data.append('name', name)
+		data.append('email', email)
+		data.append('phone', phone)
+		data.append('message', message)
+		request.send(data)
 	}
 
 })

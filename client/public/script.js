ee9464061eeff7e1eca533dcc5c5940988cc4c63
e.e.moir <e.e.moir@gmail.com>
2018-12-11 17:09:41 -0500

    added some stuff:

--- client/public/script.js
+++ client/public/script.js
@@ -1,26 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector("#form-submit").onclick = (event) => {
-		event.preventDefault()
-		const request = new XMLHttpRequest()
-		let name = document.querySelector("#name").value
-		let email = document.querySelector("#email").value
-		let phone = document.querySelector("#phone").value
-		let message = document.querySelector("#message").value
-		request.open("POST", "/", true)
-		request.onload = () => {
-			let data = request.responseText
-			if (data.success) {
-				document.location.reload()
-				document.querySelector("#contact-form").innerHTML = "Your form has been successfully submitted!"
+	let button = document.querySelector("#form-submit")
+	if (button) {
+		button.onclick = (event) => {
+			event.preventDefault()
+			const request = new XMLHttpRequest()
+			let name = document.querySelector("#name").value
+			let email = document.querySelector("#email").value
+			let phone = document.querySelector("#phone").value
+			let message = document.querySelector("#message").value
+			request.open("POST", "/", true)
+			request.onload = () => {
+				let data = request.responseText
+				if (data.success) {
+					document.location.reload()
+					document.querySelector("#contact-form").innerHTML = "Your form has been successfully submitted!"
+				}
 			}
+			const data = new FormData()
+			data.append('name', name)
+			data.append('email', email)
+			data.append('phone', phone)
+			data.append('message', message)
+			request.send(data)
+			return false
 		}
-		const data = new FormData()
-		data.append('name', name)
-		data.append('email', email)
-		data.append('phone', phone)
-		data.append('message', message)
-		request.send(data)
-		return false
 	}
-
 })

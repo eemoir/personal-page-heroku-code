e6a7b5b07845098eec8aa4cdb81c1e42f0290261
e.e.moir <e.e.moir@gmail.com>
2018-12-14 13:22:10 -0500

    fucking json

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('*', (req,res) =>{
 });
 
 app.post('/contact', (req,res) => {
-	let data = JSON.parse(req.body)
+	let data = req.body
 	let message = new Message({
 		name: data.name,
 		phone: data.phone,

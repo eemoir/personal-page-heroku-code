73706a8a584a464f9a19dba6a5fc46217cd81011
e.e.moir <e.e.moir@gmail.com>
2019-02-15 12:22:46 -0500

    changed about page

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -6,17 +6,23 @@ export default function About(props) {
 	return (
 		<FadeIn delay={400}>
 			<div aria-labelledby="ABOUT">
-			<p><img id="portrait" src={portrait} align="left" />Welcome! My name is Erin, and I'm a web and mobile app developer. 
-		I've worked on both front and back end web development projects, with a focus 
-		on front-end JavaScript in a Node or Django context, as well as mobile application 
-		development with React Native 
+			<p><img id="portrait" src={portrait} align="left" />Welcome! My name is Erin, and I'm a fullstack web and mobile application developer. 
+			Although I've worked on both front and back end projects, my focus is 
+		on front-end JavaScript, as well as mobile application development with React Native 
 		and Redux. I'm fluent in JavaScript, Python, PHP, and HTML, conversant in Ruby 
 		and C, and have worked with the following libraries and frameworks, among others: 
-		Node, 
-		Django, Flask, Laravel, Express, SQL, MongoDB, React/React Native, Redux, CSS/Sass, 
-		jQuery, Ajax, and Jest.</p>
+		Node, Laravel, Flask, Django, Express, MySQL, MongoDB, React/React Native, Redux, CSS/Sass, Bootstrap, jQuery, and Jest.</p>
 
-		<p>One of my favorite things about programming is the sheer number and quality of 
+		<p>One of my favorite things about programming is the way it lets me combine skill, creativity and problem-solving 
+			to build helpful tools that other people can use. When my partner, Dr. Andy Weisenfeld, 
+			had an idea for an application that could make his job as a veterinarian easier by abstracting away some tedious 
+			and cumbersome calculations, I was more than happy to help out. Together we founded <a target="_blank" href="https://monotremestudios.com">Montreme Studios, 
+		LLC</a>, which currently has two mobile applications for veterinarians in development and another in the brainstorming stages. On the web development side, the project I've built that I'm most excited about is a meditation app called 
+		<a href="https://dhammahelper.com" target="_blank">Dhamma Helper</a>. It uses a Laravel backend and several JavaScript libraries on the frontend
+		to deliver a meditation app with text and audio diaries, account metrics, and social sharing, among other features. For more technical information about it, 
+		please see the project's <a target="_blank" href="#">README page on GitHub</a>. I'm currently developing this project as a React Native mobile application, as well.</p>
+
+		<p>One of my other favorite things about programming is the sheer number and quality of 
 		open-source resources available, which have been particularly helpful for a
 		self-taught programmer like me. Some of my favorite sources of coding knowledge are 
 		Harvard CS50's

21f5e1ed304344ffc48a53e1144625a985203034
e.e.moir <e.e.moir@gmail.com>
2019-02-18 12:03:00 -0500

    changed styling of copyright again

--- client/src/components/copyright.js
+++ client/src/components/copyright.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Copyright(props) {
     return (
-        <div style={{textAlign: 'center'}}><small>&copy; 2019 Monotreme Studios, LLC.</small></div>
+        <div id="copyright"><small>&copy; 2019 Monotreme Studios, LLC.</small></div>
     )
 } 

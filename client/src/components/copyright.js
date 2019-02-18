da126a86a8491f455086257241c9f4b4a2b52c6d
e.e.moir <e.e.moir@gmail.com>
2019-02-18 12:05:05 -0500

    added source code link to copyright

--- client/src/components/copyright.js
+++ client/src/components/copyright.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Copyright(props) {
     return (
-        <div id="copyright"><small>&copy; 2019 Monotreme Studios, LLC.</small></div>
+        <div id="copyright"><small>&copy; 2019 Monotreme Studios, LLC. View source code on <a href="https://github.com/eemoir/personal-page-heroku-code" target="_blank" rel="noopener noreferrer">GitHub</a>.</small></div>
     )
 } 

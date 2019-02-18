f118a32d788d85ec5a1841a8b6e23ac91718b0c2
e.e.moir <e.e.moir@gmail.com>
2019-02-18 11:48:16 -0500

    fixed style error

--- client/src/components/copyright.js
+++ client/src/components/copyright.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Copyright(props) {
     return (
-        <div style="text-align: center"><small>&copy; 2019 Monotreme Studios, LLC.</small></div>
+        <div style={{textAlign: center}}><small>&copy; 2019 Monotreme Studios, LLC.</small></div>
     )
 } 
